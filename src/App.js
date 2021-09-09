c07bbd686b0edfc79c7069c5d4cdb81b9a47df4e
thinhxd12 <thinhxd12@gmail.com>
2021-09-09 20:03:22 +0700

    admin_template

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './pages/Home/Home';
 import { HomeTemplate } from './templates/HomeTemplate';
 //Thư viện giúp chuyển hướng trang ở các file không phải là component
 import { createBrowserHistory } from 'history'
+import { AdminTemplate } from './templates/AdminTemplate';
+import Films from './pages/_Admin/Films/Films'
+import AddFilm from './pages/_Admin/AddFilms/AddFilm';
 // import BaiTapTongHop from './components/BaiTapTongHop/BaiTapTongHop';
 
 export const history = createBrowserHistory();
@@ -17,8 +20,9 @@ function App() {
 
         <HomeTemplate path="/home" component={Home} />
 
-        {/* <HomeTemplate exact path="/baitaptonghop" component={BaiTapTongHop} /> */}
-
+        <AdminTemplate exact path="/admin/films" component={Films} />
+        <AdminTemplate exact path="/admin/addfilm" component={AddFilm} />
+        <AdminTemplate exact path="/admin" component={Home} />
 
         <HomeTemplate exact path="/" component={Home} />
       </Switch>
---
+++ src/templates/AdminTemplate.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useState } from "react"
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import { NavLink, Route } from "react-router-dom";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+export const AdminTemplate = (props) => {
+    const [state, setState] = useState({
+        collapsed: false,
+    })
+
+
+    const onCollapse = collapsed => {
+        setState({ collapsed });
+    };
+
+    return <Route exact path={props.path} render={(propsRoute) => {
+        return <Fragment>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider collapsible collapsed={state.collapsed} onCollapse={onCollapse}>
+                    <div className="logo" />
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <Menu.Item key="1" icon={<PieChartOutlined />}>
+                            User
+                        </Menu.Item>
+                        <SubMenu key="sub1" icon={<UserOutlined />} title="Films">
+                            <Menu.Item key="3">
+                                <NavLink to="/admin/films">
+                                    Films
+                                </NavLink>
+                            </Menu.Item>
+                            <Menu.Item key="4">
+                                <NavLink to="/admin/addfilm">
+                                    Add Film
+                                </NavLink>
+                            </Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="9" icon={<FileOutlined />}>
+                            Show time
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }} />
+                    <Content style={{ margin: '16px' }}>
+                        {/* <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb> */}
+                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+
+                            <props.component {...propsRoute} />
+
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
+            </Layout>
+
+        </Fragment>
+
+    }} />
+
+}
